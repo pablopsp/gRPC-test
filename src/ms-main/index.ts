c78399fb57821bc08c25344b3adda1283b72f961
pablopsp <pablosilio@gmail.com>
2023-08-04 20:01:24 +0200

    feat: add all the rpc ms fn to express

--- src/ms-main/index.ts
+++ src/ms-main/index.ts
@@ -1,8 +1,18 @@
-import express, { json } from "express";
-import { credentials } from "@grpc/grpc-js";
+import express, { Request, Response, json } from "express";
+import { ServiceError, credentials } from "@grpc/grpc-js";
 
 import { WalletClient } from "@/protos/wallet_grpc_pb";
-import { CreateAddressRequest } from "@/protos/wallet_pb";
+import {
+  BalanceRequest,
+  BalanceResponse,
+  CreateAddressRequest,
+  CreateAddressResponse,
+  Transaction,
+  TransactionRequest,
+  TransactionResponse,
+  WalletInfoRequest,
+  WalletInfoResponse,
+} from "@/protos/wallet_pb";
 
 const walletClient = new WalletClient(
   "localhost:50051",
@@ -12,11 +22,64 @@ const walletClient = new WalletClient(
 const app = express();
 app.use(json());
 
-app.get("/address", (req, res) => {
-  walletClient.createAddress(new CreateAddressRequest(), (err, resp) => {
-    res.send(resp.getAddress());
-  });
+app.get("/address/create", (req: Request, res: Response) => {
+  walletClient.createAddress(
+    new CreateAddressRequest(),
+    (err: ServiceError, resp: CreateAddressResponse) => {
+      const address = resp.toObject();
+      res.json(address);
+    }
+  );
 });
+
+app.get("/address/:addressId/balance", (req: Request, res: Response) => {
+  const balanceRequest = new BalanceRequest();
+  balanceRequest.setAddress(req.params.addressId);
+
+  walletClient.balance(
+    balanceRequest,
+    (err: ServiceError, resp: BalanceResponse) => {
+      const balance = resp.toObject();
+      res.json(balance);
+    }
+  );
+});
+
+app.get("/address/:addressId/wallet-info", (req: Request, res: Response) => {
+  const walletInfoRequest = new WalletInfoRequest();
+  walletInfoRequest.setAddress(req.params.addressId);
+
+  walletClient.walletInfo(
+    walletInfoRequest,
+    (err: ServiceError, resp: WalletInfoResponse) => {
+      const info = resp.toObject();
+      res.json(info);
+    }
+  );
+});
+
+app.post(
+  "/address/:addressFrom/transaction/:addressTo",
+  (req: Request, res: Response) => {
+    const transactionRequest = new TransactionRequest();
+    const transaction = new Transaction();
+
+    transaction.setFromAddress(req.params.addressFrom);
+    transaction.setToAddress(req.params.addressTo);
+    transaction.setValue(req.body.value);
+    transaction.setDate(new Date().toUTCString());
+
+    transactionRequest.setTransaction(transaction);
+
+    walletClient.transaction(
+      transactionRequest,
+      (err: ServiceError, resp: TransactionResponse) => {
+        const info = resp.toObject();
+        res.json(info);
+      }
+    );
+  }
+);
 
 const port = 5000;
 app.listen(port, () => {
